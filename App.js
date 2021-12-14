a7413739532de25d0fb5bef418f438dec6503e8e
joseph-benoy <josephbenoy03@gmail.com>
2021-12-14 17:46:10 +0530

    btc fetching added

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import React from "react";
+import axios from 'axios';
 export default function App() {
+  const [btc,setBtc] = React.useState("Test");
+  const [eth,setEth] = React.useState("");
+  const[dog,setCoin] = React.useState("");
+  const[error,setError] = React.useState("");
+  React.useEffect(async ()=>{
+    try{
+        const res = await axios({
+          method: 'GET',
+          url: 'https://rest.coinapi.io/v1/exchangerate/BTC/INR',
+          headers: {'X-CoinAPI-Key': '20541593-BB85-46BC-BF80-FEAA04B43B0D'}
+      });
+      setBtc(res.data.rate);
+    }
+    catch(e){
+		setBtc(e.message)
+    }
+  },[]);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{btc}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,6 +33,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent:'center'
   },
 });
